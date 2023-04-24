e8e60d69c8db794fd5c94eafbcc560a67c4488e6
Firuz <razackoff@gmail.com>
2023-04-25 00:07:39 +0500

    Создать контроллер ошибок.

---
+++ Controllers/ErrorsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace ShoppingListApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ErrorsController : ControllerBase
+    {
+        [Route("/error-development")]
+        public IActionResult HandleErrorDevelopment([FromServices] IHostEnvironment hostEnviroment, HttpContext httpContext)
+        {
+            if(!hostEnviroment.IsDevelopment())
+            {
+                return NotFound();
+            }
+            var exceptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            return Problem(detail: exceptionHandlerFeature.Error.StackTrace,
+                title: exceptionHandlerFeature.Error.Message);
+
+        }
+        [Route("Error")]
+        public IActionResult HandleError()
+        {
+            return Problem();
+        }
+
+        [HttpGet("Throw")]
+        public IActionResult Throw() 
+        {
+            throw new Exception("Sample Exception");
+        }
+    }
+
+}
